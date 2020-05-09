9d62a7b1e661bdfac976470c318d247aa8758e3a
jaouadballat <jaouad.ballat@regily.com>
2020-05-09 21:26:27 +0000

    apply filter for posted dates

--- public/js/app.js
+++ public/js/app.js
@@ -66587,53 +66587,58 @@ var withLoader = function withLoader(Component) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _services_helpers__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./services/helpers */ "./resources/js/components/services/helpers.js");
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+
 
 
 var PostedDate = function PostedDate(_ref) {
   var onChange = _ref.onChange;
+
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(Object(_services_helpers__WEBPACK_IMPORTED_MODULE_1__["valueFromUrlParams"])('posted_date') || ''),
+      _useState2 = _slicedToArray(_useState, 2),
+      value = _useState2[0],
+      setValue = _useState2[1];
+
+  var dates = ['Any', 'Today', 'Last 2 days', 'Last 3 days', 'Last 5 days', 'Last 10 days'];
+
+  var onChangeHandler = function onChangeHandler(event) {
+    var value = event.target.id;
+    setValue(value);
+    Object(_services_helpers__WEBPACK_IMPORTED_MODULE_1__["appendParamsToUrl"])('posted_date', value);
+    Object(_services_helpers__WEBPACK_IMPORTED_MODULE_1__["appendParamsToUrl"])('page', 1);
+    onChange(event);
+  };
+
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
     className: "select-Categories pb-50"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
     className: "small-section-tittle2"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "Posted Within")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {
-    className: "container"
-  }, "Any", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
-    type: "checkbox",
-    onChange: onChange
-  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
-    className: "checkmark"
-  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {
-    className: "container"
-  }, "Today", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
-    type: "checkbox",
-    checked: "checked active"
-  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
-    className: "checkmark"
-  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {
-    className: "container"
-  }, "Last 2 days", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
-    type: "checkbox"
-  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
-    className: "checkmark"
-  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {
-    className: "container"
-  }, "Last 3 days", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
-    type: "checkbox"
-  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
-    className: "checkmark"
-  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {
-    className: "container"
-  }, "Last 5 days", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
-    type: "checkbox"
-  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
-    className: "checkmark"
-  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {
-    className: "container"
-  }, "Last 10 days", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
-    type: "checkbox"
-  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
-    className: "checkmark"
-  })));
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "Posted Within")), dates.map(function (date) {
+    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {
+      className: "container"
+    }, date, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+      type: "checkbox",
+      name: "posted_date",
+      id: date,
+      value: value,
+      checked: date === value && 'checked',
+      onChange: onChangeHandler
+    }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+      className: "checkmark"
+    }));
+  }));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (PostedDate);
--- resources/js/components/PostedDate.jsx
+++ resources/js/components/PostedDate.jsx
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {appendParamsToUrl, valueFromUrlParams} from "./services/helpers";
 
-const PostedDate = ({onChange}) => (
-    <div className="select-Categories pb-50">
-        <div className="small-section-tittle2">
-            <h4>Posted Within</h4>
+const PostedDate = ({onChange}) => {
+
+    const [value, setValue] = useState(valueFromUrlParams('posted_date') || '');
+
+
+    const dates  = [
+        'Any',
+        'Today',
+        'Last 2 days',
+        'Last 3 days',
+        'Last 5 days',
+        'Last 10 days',
+    ];
+    const onChangeHandler = event => {
+        const { id: value } = event.target;
+        setValue(value);
+        appendParamsToUrl('posted_date', value);
+        appendParamsToUrl('page', 1);
+        onChange(event);
+    }
+
+    return (
+        <div className="select-Categories pb-50">
+            <div className="small-section-tittle2">
+                <h4>Posted Within</h4>
+            </div>
+            {dates.map(date => (
+                <label className="container">{date}
+                    <input type="checkbox" name='posted_date' id={date} value={value} checked={date === value && 'checked'} onChange={onChangeHandler} />
+                    <span className="checkmark"></span>
+                </label>
+            ))}
+
+
         </div>
-        <label className="container">Any
-            <input type="checkbox" onChange={onChange} />
-            <span className="checkmark"></span>
-        </label>
-        <label className="container">Today
-            <input type="checkbox" checked="checked active" />
-            <span className="checkmark"></span>
-        </label>
-        <label className="container">Last 2 days
-            <input type="checkbox" />
-            <span className="checkmark"></span>
-        </label>
-        <label className="container">Last 3 days
-            <input type="checkbox" />
-            <span className="checkmark"></span>
-        </label>
-        <label className="container">Last 5 days
-            <input type="checkbox" />
-            <span className="checkmark"></span>
-        </label>
-        <label className="container">Last 10 days
-            <input type="checkbox" />
-            <span className="checkmark"></span>
-        </label>
-    </div>
-);
+    );
+}
 
 export default PostedDate;
