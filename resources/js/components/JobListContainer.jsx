b733b7897f9f173602c3491da062b93799833d0d
jaouadballat <jaouad.ballat@regily.com>
2020-05-12 18:53:54 +0000

    call ajax when filter change

--- resources/js/components/JobListContainer.jsx
+++ resources/js/components/JobListContainer.jsx
@@ -3,17 +3,16 @@ import JobList from './JobList'
 import { useFetch } from './services/useFetch'
 import {withLoader} from "./LoaderHoc";
 import {getFilterFromUrlParams} from "./services/helpers";
+import {getJobsList} from "./store/actions";
 
 const JobListContainer = () => {
 
     const  [{jobs, categories, isLoading}, dispatch] = useFetch();
     const WithLoader = withLoader(JobList);
 
-    const handleChangeEvent = e => {
-        const {name, value, type, id} = e.target;
-        console.log({name, value, type, id})
-        console.log({filter: getFilterFromUrlParams()})
-
+    const handleChangeEvent = () => {
+        const filter = getFilterFromUrlParams();
+        getJobsList(dispatch, filter);
     }
 
     const props = {
