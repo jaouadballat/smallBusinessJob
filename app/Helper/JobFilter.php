5069a17ea02f320c916ba6bdbbfea0b16435c5e7
jaouadballat <jaouad.ballat@regily.com>
2020-05-15 19:36:03 +0000

    fix posted date filter

--- app/Helper/JobFilter.php
+++ app/Helper/JobFilter.php
@@ -9,6 +9,7 @@ use App\Models\Job;
 use App\Repositories\BaseRepositoryInterface;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class JobFilter
 {
@@ -32,8 +33,24 @@ class JobFilter
         if($request->has('experience')) {
             if(sizeof(explode(',', $request->experience)) === 1 and (integer) $request->experience === 6) {
                 $jobs->where('experiences_number', '>=', $request->experience);
-            } else {
+            } else if(sizeof(explode(',', $request->experience)) > 1){
                 $jobs->whereBetween('experiences_number', explode(',', $request->experience));
+            } else return null;
+        }
+
+        if($request->has('posted_date')) {
+            switch($request->posted_date) {
+                case 'Today': $jobs->where('postedDate', now(), '<=');
+                    break;
+                case 'Last 2 days': $jobs->where('postedDate', '=', now()->subDays(2), '<=');
+                    break;
+                case 'Last 3 days': $jobs->where('postedDate', now()->subDays(3), '<=');
+                    break;
+                case 'Last 5 days': $jobs->where('postedDate', now()->subDays(5), '<=');
+                    break;
+                case 'Last 10 days': $jobs->where('postedDate', now()->subDays(12), '<=');
+                    break;
+                default: return null;
             }
         }
 
