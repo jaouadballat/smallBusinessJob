d9402e9b86e5f2ac28d71df963f6662c5dea3702
jaouadballat <jaouad.ballat@regily.com>
2020-05-16 20:40:56 +0000

    fix background image on home page header

--- resources/views/templats/header.blade.php
+++ resources/views/templats/header.blade.php
@@ -1,7 +1,7 @@
 <div class="slider-area ">
     <!-- Mobile Menu -->
     <div class="slider-active">
-        <div class="single-slider slider-height d-flex align-items-center" data-background="{{asset('img/hero/h1_hero.jpg')}}">
+        <div class="single-slider slider-height d-flex align-items-center" style="background-image: url({{asset('img/hero/h1_hero.jpg')}})">
             <div class="container">
                 <div class="row">
                     <div class="col-xl-6 col-lg-9 col-md-10">
