c56f82d453ee42b23e53a7e84ca77da49c57f8c6
jaouadballat <jaouad.ballat@regily.com>
2020-06-05 00:14:49 +0100

    move all freelancer routes on freelancer route file

--- routes/freelancer.php
+++ routes/freelancer.php
@@ -3,7 +3,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'FreelancerController@dashboard')->name('freelancer.dashboard')->middleware('freelancer');
-Route::get('/job/{id}', 'FreelancerController@show')->name('freelancer.show')->middleware('freelancer');
-Route::post('/job/apply/{id}', 'FreelancerController@apply')->name('freelancer.apply')->middleware('freelancer');
 
+Route::middleware('freelancer')->group(function() {
+    Route::get('/', 'FreelancerController@dashboard')->name('freelancer.dashboard');
+    Route::get('/job/{id}', 'FreelancerController@show')->name('freelancer.show');
+    Route::post('/job/apply/{id}', 'FreelancerController@apply')->name('freelancer.apply');
+});
