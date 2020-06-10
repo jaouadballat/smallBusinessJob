b5e01948659dd0ba0d577289a9546c2b1971438f
jaouadballat <jaouad.ballat@regily.com>
2020-06-10 20:00:00 +0100

    WIP

--- app/Repositories/FreelancerRepository/FreelancerRepository.php
+++ app/Repositories/FreelancerRepository/FreelancerRepository.php
@@ -36,9 +36,9 @@ class FreelancerRepository extends BaseRepository implements FreelancerRepositor
     public function create($data, $jobId)
     {
         $user = auth()->user();
-        $job = $this->jobModel->findOrFail($jobId);
+        $this->jobModel->findOrFail($jobId);
 
-        $freelancer = $user->freelancer()->updateOrCreate([
+        $user->freelancer()->updateOrCreate([
             'resume' => $data['resume']
         ]);
 
@@ -46,36 +46,7 @@ class FreelancerRepository extends BaseRepository implements FreelancerRepositor
 
         $this->messageModel->body = $data['body'];
 
-        $freelancer->messages()->create([
-            'job_id' => $jobId,
-            'agency_id' => $job->agency->id,
-            'from_id' => $user->id,
-            'body' => $data['body']
-        ]);
-
         event(new JobEvent($user, $this->messageModel));
     }
 
-    public function allPostedJob()
-    {
-        $jobIds = [];
-
-        $freelancer = auth()->user()->freelancer()->first();
-        $messages = $freelancer->messages()->get()->groupBy('job_id');
-
-        foreach ($messages as $jobId => $messages) {
-            $jobIds[] = $jobId;
-        }
-        return $this->jobModel->findMany($jobIds);
-
-    }
-
-    public function messages($jobId)
-    {
-        $job = $this->jobModel->findOrFail($jobId);
-        $freelancer = auth()->user()->freelancer()->first();
-        $agency = $job->agency;
-
-        return $freelancer->messages()->where('job_id', $jobId)->where('agency_id', $agency->id)->get();
-    }
 }
--- routes/freelancer.php
+++ routes/freelancer.php
@@ -6,9 +6,5 @@ use Illuminate\Support\Facades\Route;
 
     Route::get('/', 'FreelancerController@dashboard')->name('freelancer.dashboard');
     Route::get('/job/list', 'FreelancerController@list')->name('freelancer.list');
-    Route::get('/job/{job}/messages', 'FreelancerController@messages')->name('freelancer.messages.show');
-
     Route::get('/job/{id}', 'FreelancerController@show')->name('freelancer.show');
     Route::post('/job/apply/{id}', 'FreelancerController@apply')->name('freelancer.apply');
-    //Route::get('/job/{job}/messages', 'MessageController@show')->name('messages.show');
-//    Route::post('/job/{jobId}/messages', 'MessageController@send')->name('messages.send');
