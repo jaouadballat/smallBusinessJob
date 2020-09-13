38372181bf8a38fbaac60e662b06c394385068e7
jaouadballat <jaouad.ballat@regily.com>
2020-09-13 19:21:53 +0100

    update style of freelancer list on agency dashboard

--- resources/views/Agency/freelancer.blade.php
+++ resources/views/Agency/freelancer.blade.php
@@ -3,15 +3,26 @@
 @section('content')
     <div class="container">
         <h3>List of freelancers</h3>
-        <ul>
-            @forelse($freelancers as $freelancer)
-                <li>
-                    <a href="{{ route('agency.dashboard.messages', ['freelancerId' => $freelancer->id, 'jobId' => $id]) }}" class="text-danger">{{ $freelancer->user()->first()->name }}</a>
-                </li>
-            @empty
-                <p>no job yet</p>
-            @endforelse
-        </ul>
 
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-12">
+                    @forelse($freelancers as $freelancer)
+                        <div class="card border border-danger rounded mb-5">
+                            <img class="card-img-top" style="max-width: 100%; max-height: 200px" src="{{ asset($freelancer->avatar) }}" alt="{{ $freelancer->fullName() }}">
+                            <div class="card-body">
+                                <h3 class="card-title font-weight-bold m-0">{{ $freelancer->fullName() }}</h3>
+                                <p class="card-text m-0 mb-2">{{ $freelancer->title }}</p>
+                                <a href="{{ route('agency.dashboard.messages', ['freelancerId' => $freelancer->id, 'jobId' => $id]) }}" class="btn-xs btn-primary">Messages</a>
+                                <a href="{{ route('profile.show', ['id' => $freelancer->id]) }}" class="btn-xs btn-primary">Profile</a>
+                            </div>
+                        </div>
+                    @empty
+                        <p>no message yet for this job</p>
+                    @endforelse
+
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
